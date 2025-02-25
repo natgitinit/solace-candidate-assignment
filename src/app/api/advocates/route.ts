dd077c2521890c25dddaee7d9e3a9dc475371402
natgitinit <natgitinit@gmail.com>
2025-02-24 22:02:29 -0600

    db seeded

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,12 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { advocateData as seedData } from "../../../db/seed/advocates";
 
 export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+	// Uncomment this line to use a database
+	const dbData = await db.select().from(advocates);
 
-  const data = advocateData;
+	// const data = seedData;
 
-  return Response.json({ data });
+	return Response.json({ data: dbData });
 }
